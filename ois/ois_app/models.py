ce56a948af25367ae9f8ee6dacc51f199566cc50
EmileSonneveld <contact@emilesonneveld.be>
2018-11-19 12:41:13 +0100

    First view: show_accounts

--- ois/ois_app/models.py
+++ ois/ois_app/models.py
@@ -5,19 +5,27 @@ from django.db import models
 # Don't specify "human readable" names please. 
 # We are computer scientists, not humans ;)
 
+# Dont use 'pk' (primary_key) as table name!
 
 class Account(models.Model):
     email = models.CharField(max_length=500, primary_key=True)
     name = models.CharField(max_length=500)
     password = models.CharField(max_length=500)
+    def __str__(self):
+        return self.email + " name: " + self.name
 
 class Doctor(models.Model):
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     speciality = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.account_id.__str__() + " speciality: " + self.speciality[0:5]
 
 class Patient(models.Model):
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
-    data_of_birth = models.DateTimeField()
+    date_of_birth = models.DateField()
+    def __str__(self):
+        return self.account_id.__str__() + " date_of_birth: " + str(self.date_of_birth)
+
 
 class Symptom(models.Model):
     name_slug = models.CharField(max_length=500, primary_key=True)
--- ois/ois_app/urls.py
+++ ois/ois_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('show_accounts', views.show_accounts, name='show_accounts'),
 ]
