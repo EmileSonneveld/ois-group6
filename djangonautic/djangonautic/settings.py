7fbb136ffc9689cc53c103c4097562340e49f9b3
Emile Sonneveld <contact@emilesonneveld.be>
2018-12-16 22:45:10 +0100

    Symps from slides.

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'djangonautic.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #},
     'default': {
-        'NAME': 'ois',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-        'PASSWORD': 'admin'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    #'default': {
+    #    'NAME': 'ois',
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'admin'
+    #},
 }
 
 
